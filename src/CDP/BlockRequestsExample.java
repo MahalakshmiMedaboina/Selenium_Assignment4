a5b4479808f1d79d702d0cb53f248b2c627ce99a
MahalakshmiMedaboina <medaboinamahaa@gmail.com>
2026-04-15 15:12:26 +0530

    Block Requests using logs

---
+++ src/CDP/BlockRequestsExample.java
@@ -0,0 +1,33 @@
+package CDP;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class BlockRequestsExample {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver",
+                "C:\\ChromeDriver121\\chromedriver-win64\\chromedriver.exe");
+
+        ChromeOptions options = new ChromeOptions();
+        options.setBinary("C:\\Chrome121\\chrome-win64\\chrome.exe");
+
+        // ✅ Block images
+        options.addArguments("--blink-settings=imagesEnabled=false");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://www.amazon.com");
+
+        System.out.println("Page loaded without images");
+
+        try {
+            Thread.sleep(5000);
+        } catch (Exception e) {
+        }
+
+        driver.quit();
+    }
+}
